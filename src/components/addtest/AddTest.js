72e9dfe821ba82418204bac7e993e07afcd91b58
dave <dsersch640@gmail.com>
2020-02-27 08:15:06 -0800

    remove knex test

--- src/components/addtest/AddTest.js
+++ src/components/addtest/AddTest.js
@@ -1,27 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './AddTest.css';
 
-const AddTest = () => {
-    return (
-        <div className="addTest">
-            <h2>Add Test</h2>
-            <div className="inputSection">
-                <p className="label">time</p>
-                <input type="time" className="addTestInput time"/>
-            </div>
-            <div className="inputSection">
-                <p className="label">fasting:  </p>
-                <input type="checkbox" />
-            </div>
-            <div className="inputSection">
-                <p className="label">Result</p>
-                <input type="number" className="addTestInput number"/>
-            </div>
-            <p className="notesLabel">Notes</p>
-            <input type="textbox" className="addTestInput"/>
-            <button className="addButton">Add Result</button>
-        </div> 
-    )
+class AddTest extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            fasting: '',
+            sugar: '',
+            notes: ''
+        }
+    }
+
+    onFastingChange = (event) => {
+        this.setState({fasting: event.target.value})
+    }
+
+    onSugarChange = (event) => {
+        this.state({sugar: event.target.value})
+    }
+
+    onNotesChange = (event) => {
+        this.state({notes: event.target.value})
+    }
+
+    render() {
+        return (
+            <div className="addTest">
+                <h2>Add Test</h2>
+                <div className="inputSection">
+                    <p className="label">fasting:  </p>
+                    <input onChange={this.onFastingChange} type="checkbox" />
+                </div>
+                <div className="inputSection">
+                    <p className="label">Result</p>
+                    <input onChange={this.onSugarChange} type="number" className="addTestInput number"/>
+                </div>
+                <p className="notesLabel">Notes</p>
+                <input onChange={this.onNotesChange} type="textbox" className="addTestInput"/>
+                <button className="addButton">Add Result</button>
+            </div> 
+        )
+    }  
 }
 
 export default AddTest;
